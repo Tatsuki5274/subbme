e3c3c45d50304845225563888b0d7f1bb70d6e38
Tatsuki <private.1.5810@fastmail.jp>
2021-03-28 15:26:03 +0900

    ソートの実装（未完成）

--- src/components/service/organisms/ServiceListFunction.tsx
+++ src/components/service/organisms/ServiceListFunction.tsx
@@ -4,36 +4,74 @@ import React from "react";
 import { getServiceUnitString, getServiceUnitValue } from "repositories/Services";
 const { Option } = Select;
 
-export type ServiceListFunctionType = {
-    serviceName: string
-    planName: string
-    unit: string
-    unitTerm: string
-    costPerDay: number
-    currency: string
-}
+// export type ServiceListFunctionType = {
+//     serviceName: string
+//     planName: string
+//     unit: string
+//     unitTerm: string
+//     costPerDay: number
+//     currency: string
+// }
 
 type PropsType = {
     setUnit: React.Dispatch<React.SetStateAction<ServiceUnitType>>
-}
+    serviceList: Service[] | null,
+    setServiceList: React.Dispatch<React.SetStateAction<Service[] | null>>}
 
 /**
  * @returns 
  */
 export default function ServiceListFunction(props: PropsType){
+    const sortSelection = [
+        {
+            display: "価格の高い順",
+            value: "cost"
+        },
+        {
+            display: "新しい準",
+            value: "new"
+        }
+    ]
     return (
-        <Select
-            placeholder="Select a option and change input text above"
-            onChange={(value) => {
-                props.setUnit(value);
-            }}
-            defaultValue={ServiceUnitEnum.Month}
-        >
-            {Object.values(ServiceUnitEnum).map(unit => {
-                return (
-                    <Option value={unit}>{getServiceUnitString(unit)}</Option>
-                )
-            })}
-      </Select>
+        <>
+            <Select
+                onChange={(value) => {
+                    props.setUnit(value);
+                }}
+                defaultValue={ServiceUnitEnum.Month}
+            >
+                {Object.values(ServiceUnitEnum).map(unit => {
+                    return (
+                        <Option value={unit}>{getServiceUnitString(unit)}</Option>
+                    )
+                })}
+            </Select>
+            <Select
+                onChange={(value) => {
+                    const serviceList = props.serviceList;
+                    switch(value){
+                        case "new":
+                            serviceList?.sort((a, b) => {
+                                return a.createdAt && b.createdAt && (a.createdAt < b.createdAt) ? 1 : -1
+                            })
+                            break;
+                        case "cost":
+                            serviceList?.sort((a, b) => {
+                                return a.costPerDay && b.costPerDay && (a.costPerDay < b.costPerDay) ? 1 : -1
+                            })
+                    }
+                    if(serviceList){
+                        props.setServiceList(serviceList.concat())
+                    }
+                }}
+                defaultValue={sortSelection[0].value}
+            >
+                {sortSelection.map(sel => {
+                    return (
+                        <Option value={sel.value}>{sel.display}</Option>
+                    )
+                })}
+            </Select>
+        </>
     );
 }
--- src/components/service/templates/ServiceListTemplate.tsx
+++ src/components/service/templates/ServiceListTemplate.tsx
@@ -7,14 +7,17 @@ import ServiceListData from "../organisms/ServiceListData";
 import ContentWrapper from "components/wrapper/ContentWrapper"
 import styled from "styled-components";
 import Text from "components/common/atoms/Text";
-import ServiceListFunction, { ServiceListFunctionType } from "../organisms/ServiceListFunction";
-import { ServiceUnitType } from "entities/Service";
+import ServiceListFunction from "../organisms/ServiceListFunction";
+import { Service, ServiceUnitType } from "entities/Service";
 
 
 type PropsType = {
     formattedTotalCost: string
     cardData: ServiceListCardType[] | null
     setUnit: React.Dispatch<React.SetStateAction<ServiceUnitType>>
+
+    serviceList: Service[] | null,
+    setServiceList: React.Dispatch<React.SetStateAction<Service[] | null>>
 }
 
 export default function ServiceListTemplate(props: PropsType){
@@ -27,6 +30,8 @@ export default function ServiceListTemplate(props: PropsType){
                     <TotalCostStyle><Text>{props.formattedTotalCost}</Text></TotalCostStyle>
                     <ServiceListFunction
                         setUnit={props.setUnit}
+                        serviceList={props.serviceList}
+                        setServiceList={props.setServiceList}
                     />
                     {props.cardData ?
                     <ServiceListData
