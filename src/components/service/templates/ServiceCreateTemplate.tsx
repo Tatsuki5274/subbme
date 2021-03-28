3030602476ee866d219277f2fdcd3a44c2abd4b3
Tatsuki <private.1.5810@fastmail.jp>
2021-03-28 16:25:45 +0900

    ずれの修正

--- src/components/service/templates/ServiceCreateTemplate.tsx
+++ src/components/service/templates/ServiceCreateTemplate.tsx
@@ -1,15 +1,19 @@
 import Title from "components/common/atoms/Title";
 import Footer from "components/common/organisms/Footer";
 import Header from "components/common/organisms/Header";
+import ContentWrapper from "components/wrapper/ContentWrapper";
 import ServiceCreateForm from "../organisms/ServiceCreateForm";
 
 export default function ServiceCreateTemplate(){
     return (
-        <>
-            <Header />
-            <Title>サービス登録</Title>
-            <ServiceCreateForm />
-            <Footer />
-        </>
+        <ContentWrapper>
+            <>
+                <Header />
+                <Title>サービス登録</Title>
+                <ServiceCreateForm />
+                <Footer />
+            </>
+        </ContentWrapper>
+
     );
 }
