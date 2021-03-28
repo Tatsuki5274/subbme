17f9d9a771f12df35538b70fd44ca9149cda9dab
Tatsuki <private.1.5810@fastmail.jp>
2021-03-28 16:18:39 +0900

    ヘッダーメニューの実装

--- src/components/common/organisms/Header.tsx
+++ src/components/common/organisms/Header.tsx
@@ -1,21 +1,192 @@
+import Sider from "components/common/organisms/Menu";
 import styled from "styled-components";
 import Text from "../atoms/Text";
 
-export default function Header(){
-    return <Style>
-        <Title><Text>Subbme</Text></Title>
+// export default function Header(){
+//     return <Style>
+//         <Sider />
+//         <Title><Text>Subbme</Text></Title>
         
-    </Style>;
+//     </Style>;
+// }
+
+// const Style = styled.div({
+//     backgroundColor: "#5794C3",
+//     height: "60px"
+// })
+
+// const Title = styled.span({
+//     color: "#FFFFFF",
+//     fontSize: "32px",
+//     display: "inline-block",
+//     marginLeft: "10px"
+// })
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import SettingsIcon from '@material-ui/icons/Settings';
+import PaymentIcon from '@material-ui/icons/Payment';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function Header(props: any) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+          <ListItem>
+              <ListItemIcon><PaymentIcon /></ListItemIcon>
+              <ListItemText primary="サービス一覧" />
+          </ListItem>
+          <ListItem>
+              <ListItemIcon><AssessmentIcon /></ListItemIcon>
+              <ListItemText primary="分析一覧" />
+          </ListItem>
+      </List>
+      <Divider />
+      <List>
+          <ListItem button>
+              <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+              <ListItemText primary="プロフィール" />
+          </ListItem>
+          <ListItem>
+              <ListItemIcon><SettingsIcon /></ListItemIcon>
+              <ListItemText primary="設定" />
+          </ListItem>
+      </List>
+      <Divider />
+      <List>
+          <ListItem button>
+              <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+              <ListItemText primary="ログアウト" />
+          </ListItem>
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar} style={{backgroundColor: "#5794C3"}}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Subbme
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+
+    </div>
+  );
 }
 
-const Style = styled.div({
-    backgroundColor: "#5794C3",
-    height: "60px"
-})
+Header.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
 
-const Title = styled.span({
-    color: "#FFFFFF",
-    fontSize: "32px",
-    display: "inline-block",
-    marginLeft: "10px"
-})
+export default Header;
