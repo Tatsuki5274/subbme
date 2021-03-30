c8dde86afb012ec3245bb4aa7454f63abe8114d6
Tatsuki <private.1.5810@fastmail.jp>
2021-03-31 06:31:54 +0900

    依存型を切り出し

---
+++ src/libs/Types.ts
@@ -0,0 +1,6 @@
+import firebase from "libs/Firebase"
+
+// フロントエンド用型定義
+export type FirebaseReferenceType = firebase.firestore.CollectionReference<firebase.firestore.DocumentData>;
+export type FirebaseQueryType = firebase.firestore.Query<firebase.firestore.DocumentData>;
+export type FirebaseFirestoreTimestampType = firebase.firestore.Timestamp;
