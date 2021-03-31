e3e7a7994de829f9991e9a8ed35e6d03b4dd014b
Tatsuki <contactme0522@gmail.com>
2021-03-31 21:27:16 +0900

    payjsの設定を追加

--- functions/src/functions/onCreateAuth.ts
+++ functions/src/functions/onCreateAuth.ts
@@ -1,17 +1,35 @@
 // import * as admin from 'firebase-admin';
 import * as functions from "firebase-functions";
 import { UserManager } from '../repositories/Users';
+import * as Payjp from 'payjp';
 
 export default functions.auth.user().onCreate(async user => {
-    // pay.jpの顧客を作成
+    try{
+        // pay.jpの顧客を作成
+        const sk = functions.config().payjp.sk;
+        if (!(typeof sk === "string")) {
+            // 秘密鍵がfunctions configに設定されていない場合
+            throw new Error("SecretKey is not set");
+        }
 
-    // firestoreへユーザー情報を追加
-    
-    console.log("onCreated", user.uid);
+        const payjp = Payjp(sk);
+        await payjp.customers.create({
+            id: user.uid,
+        }).then(charge => {
+            console.log(JSON.stringify(charge));
+        }).catch((e: Payjp.ResponseError) => {
+            e.response?.body
+            throw new Error(e.message);
+        });
 
-    const userManager = new UserManager();
-    const result = await userManager.set({
-        uid: user.uid
-    })
-    return result;
+        // firestoreへユーザー情報を追加
+        const userManager = new UserManager();
+        const result = await userManager.set({
+            uid: user.uid
+        })
+        return result;
+    } catch(e) {
+        console.error(e);
+        throw new Error(e);
+    }
 })
