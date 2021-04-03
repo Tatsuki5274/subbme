332f25621ae0a80d5bebde8f9464861821a421e1
Tatsuki <private.1.5810@fastmail.jp>
2021-04-03 15:12:28 +0900

    ブロックリンクの追加

--- src/components/report/organisms/ReportListBox.tsx
+++ src/components/report/organisms/ReportListBox.tsx
@@ -1,10 +1,13 @@
 import BorderLine from "components/common/atoms/BorderLine";
 import GrayText from "components/common/atoms/GrayText";
+import LinkedBlock from "components/wrapper/LinkedBlock";
 import React from "react";
+import { routeBuilder } from "router";
 import styled from "styled-components";
 import CircleBox from "../molecules/CircleBox";
 
 type PropsType = {
+    reportID: string
     date: string
     cost: string
     comment: string
@@ -15,38 +18,43 @@ export type ReportListBoxType = PropsType;
 
 export default function ReportListBox(props: PropsType) {
     return (
-        <BoxStyle>
-            <TopStyle>
-                <DateStyle>
-                    <GrayText>
-                        {props.date}
-                    </GrayText>
-                </DateStyle>
-                <CostStyle>
-                    <GrayText>
-                        {props.cost}
-                    </GrayText>
-                </CostStyle>
-            </TopStyle>
-
-            <BorderLine />
-
-            <BottomStyle>
-                <CommentStyle>
-                    <GrayText>
-                        {props.comment}
-                    </GrayText>
-                </CommentStyle>
-                <CircleStyle>
-                    <CircleBox>
+        <LinkedBlock
+            to={routeBuilder.reportDetailPath(props.reportID)}
+        >
+            <BoxStyle>
+                <TopStyle>
+                    <DateStyle>
                         <GrayText>
-                            {props.score}
+                            {props.date}
                         </GrayText>
-                    </CircleBox>
-                </CircleStyle>
-            </BottomStyle>
-  
-        </BoxStyle>
+                    </DateStyle>
+                    <CostStyle>
+                        <GrayText>
+                            {props.cost}
+                        </GrayText>
+                    </CostStyle>
+                </TopStyle>
+
+                <BorderLine />
+
+                <BottomStyle>
+                    <CommentStyle>
+                        <GrayText>
+                            {props.comment}
+                        </GrayText>
+                    </CommentStyle>
+                    <CircleStyle>
+                        <CircleBox>
+                            <GrayText>
+                                {props.score}
+                            </GrayText>
+                        </CircleBox>
+                    </CircleStyle>
+                </BottomStyle>
+
+            </BoxStyle>
+        </LinkedBlock>
+
     );
 }
 
--- src/components/report/pages/ReportList.tsx
+++ src/components/report/pages/ReportList.tsx
@@ -5,6 +5,7 @@ import ReportListTemplate from "../templates/ReportListTemplate";
 export default function ReportList() {
     const mock: ReportListBoxType[] = [
         {
+            reportID: "hogehoge",
             date: "2020/01/01",
             cost: "¥15,000",
             comment: "モックコメントです",
