5b33076aabe9aa459ed1e0f6e6e3a2d70db363aa
Tatsuki <private.1.5810@fastmail.jp>
2021-04-06 08:33:57 +0900

    環境設定の分離

--- src/libs/Firebase.ts
+++ src/libs/Firebase.ts
@@ -1,5 +1,6 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
+import * as dotenv from "dotenv";
 // If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
 // import * as firebase from "firebase/app"
 
@@ -12,23 +13,33 @@ import "firebase/firestore";
 import "firebase/storage"
 import "firebase/functions"
 
+dotenv.config();
+
+const isEmulating = process.env.REACT_APP_FIREBASE_EMULATOR
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
+const projectId = process.env.REACT_APP_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const appId = process.env.REACT_APP_APP_ID;
+
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCQq-x5TxBBT4Fr4c3DvvhpOgEe4wY6rPM",
-    authDomain: "subbme-dev.firebaseapp.com",
-    projectId: "subbme-dev",
-    storageBucket: "subbme-dev.appspot.com",
-    messagingSenderId: "729779483858",
-    appId: "1:729779483858:web:19d88c3099ba968e3ae90b"
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId
 };
-
-
 
 if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig);
-    const isEmulating = window.location.hostname === "localhost";
-    if (isEmulating) {
+
+    if (isEmulating === "true") {
+        // エミュレーターでの動作の場合
         console.log("Emulating mode")
         firebase.auth().useEmulator("http://localhost:9099");
         firebase.functions().useEmulator("localhost", 5001);
