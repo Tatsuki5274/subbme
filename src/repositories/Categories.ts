411cd38038c864725f920ab66031deee766465b5
Tatsuki <private.1.5810@fastmail.jp>
2021-04-07 06:32:44 +0900

    カテゴリ情報の追加

--- src/repositories/Categories.ts
+++ src/repositories/Categories.ts
@@ -8,7 +8,7 @@ export const listCategories = () => {
 export const categoryList: Category[] = [
     {
       value: 'エンターテイメント',
-      label: 'エンターテインメント',
+      label: 'エンターテイメント',
       children: [
         {
           value: '音楽配信',
@@ -16,28 +16,54 @@ export const categoryList: Category[] = [
         },
         {
           value: '動画配信',
-          label: '動画配信'
+          label: '動画配信',
+        },
+        {
+          value: 'オンラインゲーム',
+          label: 'オンラインゲーム',
         },
         {
           value: 'その他',
-          label: 'その他'
+          label: 'その他',
         }
       ],
     },
     {
-      value: 'jiangsu',
-      label: 'Jiangsu',
+      value: '保険',
+      label: '保険',
       children: [
         {
-          value: 'nanjing',
-          label: 'Nanjing',
-          children: [
-            {
-              value: 'zhonghuamen',
-              label: 'Zhong Hua Men',
-            },
-          ],
+          value: '生命保険',
+          label: '生命保険',
         },
+        {
+          value: '養老保険',
+          label: '養老保険',
+        },
+        {
+          value: '医療保険',
+          label: '医療保険',
+        },
+        {
+          value: 'がん保険',
+          label: 'がん保険',
+        },
+        {
+          value: '自動車保険',
+          label: '自動車保険',
+        },
+        {
+          value: '学資保険',
+          label: '学資保険',
+        },
+        {
+          value: 'その他',
+          label: 'その他',
+        }
       ],
     },
+    {
+      value: 'その他',
+      label: 'その他',
+    }
 ];
