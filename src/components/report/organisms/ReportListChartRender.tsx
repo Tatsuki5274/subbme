485f762c7d26d0672d1486d939f912e2d4fd04ea
Tatsuki <private.1.5810@fastmail.jp>
2021-04-08 08:30:17 +0900

    日付の表示形式を変更

--- src/components/report/organisms/ReportListChartRender.tsx
+++ src/components/report/organisms/ReportListChartRender.tsx
@@ -1,18 +1,7 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 
-const data = {
-    labels: [ new Date(2020, 5, 1),new Date(2020, 6, 15), new Date(2020, 7)],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        fill: true,
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgba(255, 99, 132, 0.2)',
-      },
-    ],
-  }
+
   
   const options = {
     scales: {
@@ -29,7 +18,7 @@ const data = {
               unit: "month",
               time: {
                 displayFormats: {
-                    "month": "M"
+                    "day": "M/D"
                 }
               }
           }
@@ -37,21 +26,46 @@ const data = {
     },
   }
 
-export default function ReportChartRender(){
-    return (
-        <>
-            <div className='header'>
-                <h1 className='title'>Line Chart</h1>
-                <div className='links'>
-                    <a
-                        className='btn btn-gh'
-                        href='https://github.com/reactchartjs/react-chartjs-2/blob/react16/example/src/charts/Line.js'
-                    >
-                        Github Source
-                    </a>
-                </div>
-            </div>
-        <Line data={data} options={options} />
-      </>
-    );
+type PropsType = {
+  data: {
+    date: Date,
+    score: number,
+  }[]
+}
+
+export default function ReportChartRender(props: PropsType){
+  const data = {
+    // labels: [ new Date(2020, 5, 1),new Date(2020, 6, 15), new Date(2020, 7)],
+    labels: props.data.map(dat => {
+      return dat.date;
+    }),
+    datasets: [
+      {
+        label: '# of Votes',
+        // data: [12, 19, 3, 5, 2, 3],
+        data: props.data.map(dat => {
+          return dat.score;
+        }),
+        fill: true,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
+  }
+  return (
+      <>
+          {/* <div className='header'>
+              <h1 className='title'>Line Chart</h1>
+              <div className='links'>
+                  <a
+                      className='btn btn-gh'
+                      href='https://github.com/reactchartjs/react-chartjs-2/blob/react16/example/src/charts/Line.js'
+                  >
+                      Github Source
+                  </a>
+              </div>
+          </div> */}
+      <Line data={data} options={options} />
+    </>
+  );
 }
