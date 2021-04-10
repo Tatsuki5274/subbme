87d2ff6e2a46b284af07857a395f23998a765b18
Tatsuki <private.1.5810@fastmail.jp>
2021-04-10 18:28:26 +0900

    メールの基本型を修正

--- src/entities/Mail.ts
+++ src/entities/Mail.ts
@@ -4,20 +4,30 @@ import { NullablePartial } from "../libs/Util";
 type MailBase = {
     id: string
 
-    // 宛先メールアドレスを直接指定
-    to: string
+    from?: string
 
-    // UserコレクションのドキュメントIDのemailフィールドへ送信
-    toUids: string
-    ccUids: string
-    bccUids: string
+    // 直接かUserのドキュメントIDのどちらかが必須
+     // 宛先メールアドレスを直接指定する場合
+    to?: string
+    cc?: string
+    bcc?: string
+    replyTo?: string
+     // UserコレクションのドキュメントIDのemailフィールドへ送信する場合
+    toUids?: string[]
+    ccUids?: string[]
+    bccUids?: string[]
 
-    // 送信メッセージ
-    message: {
+    // 送信メッセージ どちらか必須
+     // 本文を直接指定する場合
+    message?: {
         subject: string
         text: string
     }
-
-    createdAt: FirebaseFirestoreTimestampType
-    updatedAt: FirebaseFirestoreTimestampType
+     // 本文をテンプレートから指定する場合
+    template?: {
+        name: string
+        data?: {
+            [key: string] : string
+        }
+    }
 }
