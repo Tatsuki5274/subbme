960927a3bf56df3cc99026228dc763063aad97ff
Tatsuki <private.1.5810@fastmail.jp>
2021-04-11 12:37:27 +0900

    lintの共通化

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
+    // "google",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
