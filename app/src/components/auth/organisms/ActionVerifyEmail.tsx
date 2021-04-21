fcf0baf2407f6e4df35a60d843c58a2a49a9bfc5
Tatsuki <private.1.5810@fastmail.jp>
2021-04-22 08:54:01 +0900

    メールアドレスの確認処理を追加

--- app/src/components/auth/organisms/ActionVerifyEmail.tsx
+++ app/src/components/auth/organisms/ActionVerifyEmail.tsx
@@ -1,7 +1,35 @@
+import Result500 from "components/common/organisms/500";
+import LoadingScreen from "components/common/organisms/LoadingScreen";
+import { auth } from "libs/Types";
+import { useEffect, useState } from "react";
+
+function useVerify(code: string) {
+  const [status, setStatus] = useState<"SUCCESS" | "FAILD" | "LOADING">(
+    "LOADING"
+  );
+  useEffect(() => {
+    (async () => {
+      try {
+        await auth.applyActionCode(code);
+        setStatus("SUCCESS");
+      } catch (e) {
+        setStatus("FAILD");
+      }
+    })();
+  }, [code]);
+  return {
+    status,
+  };
+}
+
 export default function ActionVerifyEmail(props: {
   actionCode: string;
   continueUrl: string | null;
   lang: string;
 }) {
-  return null;
+  const { status } = useVerify(props.actionCode);
+  if (status === "LOADING") return <LoadingScreen />;
+  else if (status === "SUCCESS") return <span>success!</span>;
+  else if (status === "FAILD") return <span>faild...</span>;
+  return <Result500 />;
 }
