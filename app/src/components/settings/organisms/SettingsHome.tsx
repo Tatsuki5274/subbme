458fea7e2618695a3fa1e9c4629d5386cca0fd2f
Tatsuki <private.1.5810@fastmail.jp>
2021-04-24 12:32:44 +0900

    メールアドレス変更処理の作成

--- app/src/components/settings/organisms/SettingsHome.tsx
+++ app/src/components/settings/organisms/SettingsHome.tsx
@@ -12,20 +12,22 @@ import { messageAuth } from "common/lang";
 import { useForm } from "antd/lib/form/Form";
 import AsyncButton from "components/common/atoms/AsyncButton";
 
-type PasswordFormType = {
-  currentPassword: string;
-  newPassword: string;
-  newPasswordConfirm: string;
-};
-
 export default function SettingsHome() {
+  const { currentUser } = useUser();
   const modalPassword = useModal();
+  const modalEmail = useModal();
   return (
     <>
       <Title>設定</Title>
       <SubTitle>認証情報</SubTitle>
       <RowsWrapperStyle>
-        <SettingsRow label="メールアドレス" value="hoge@subbme.com" disabled />
+        <SettingsRow
+          label="メールアドレス"
+          value={`${currentUser?.email || ""}(${
+            currentUser?.emailVerified ? "確認済み" : "未確認"
+          })`}
+          onClick={modalEmail.handleOpen}
+        />
         <SettingsRow
           onClick={modalPassword.handleOpen}
           label="パスワード"
@@ -36,6 +38,10 @@ export default function SettingsHome() {
         <ModalUpdatePassword
           visible={modalPassword.isVisible}
           handleClose={modalPassword.handleClose}
+        />
+        <ModalUpdateEmail
+          visible={modalEmail.isVisible}
+          handleClose={modalEmail.handleClose}
         />
       </div>
     </>
@@ -55,6 +61,11 @@ function ModalUpdatePassword(props: {
   visible: boolean;
   handleClose: () => void;
 }) {
+  type PasswordFormType = {
+    currentPassword: string;
+    newPassword: string;
+    newPasswordConfirm: string;
+  };
   const { currentUser } = useUser();
   const [form] = useForm<PasswordFormType>();
   const minLengthPassword = 7;
@@ -140,6 +151,89 @@ function ModalUpdatePassword(props: {
           ]}
         >
           <Input.Password />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+}
+
+function ModalUpdateEmail(props: {
+  visible: boolean;
+  handleClose: () => void;
+}) {
+  // メールアドレス変更フォームの型
+  type FormFieldType = {
+    currentPassword: string;
+    newEmail: string;
+  };
+  const { currentUser } = useUser();
+  const [form] = useForm<FormFieldType>();
+  const handleOK = async () => {
+    await form.validateFields();
+    await handleSubmit(form.getFieldsValue());
+  };
+  const handleCancel = () => {
+    form.resetFields();
+    props.handleClose();
+  };
+  const handleSubmit = async (values: FormFieldType) => {
+    if (!currentUser?.email) {
+      // ユーザーのemailが取得できない場合
+      throw new Error("User is not signedin.");
+    }
+    const credential = firebase.auth.EmailAuthProvider.credential(
+      currentUser.email,
+      values.currentPassword
+    );
+    try {
+      await currentUser.reauthenticateWithCredential(credential);
+      await currentUser.updateEmail(values.newEmail);
+      await currentUser.sendEmailVerification();
+      message.success("パスワードの変更が成功しました");
+      form.resetFields();
+      props.handleClose();
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.warn(e.message, e.code);
+      message.warn(messageAuth(e));
+    }
+  };
+
+  return (
+    <Modal
+      title="メールアドレス変更"
+      onOk={handleOK}
+      onCancel={handleCancel}
+      visible={props.visible}
+      footer={[
+        <Button key="cancel" onClick={handleCancel}>
+          キャンセル
+        </Button>,
+        <AsyncButton key="ok" type="primary" onClick={handleOK}>
+          OK
+        </AsyncButton>,
+      ]}
+    >
+      <Form<FormFieldType> form={form}>
+        <Form.Item
+          label="現在のパスワード"
+          name="currentPassword"
+          rules={[{ required: true, message: "入力してください" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="変更後のメールアドレス"
+          name="newEmail"
+          rules={[
+            { required: true, message: "入力してください" },
+            {
+              type: "email",
+              message: "正しい形式で入力してください",
+            },
+          ]}
+        >
+          <Input type="email" />
         </Form.Item>
       </Form>
     </Modal>
