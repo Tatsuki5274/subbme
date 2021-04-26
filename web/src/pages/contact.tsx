b9f874e6cede34d079be51b2f424d97d5b3c5c67
Tatsuki <private.1.5810@fastmail.jp>
2021-04-26 21:31:21 +0900

    contactの雛形を作成

---
+++ web/src/pages/contact.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export default function Contact() {
+  return (
+    <>
+      <Helmet>
+        <html lang="ja" />
+        <title>Contact | Subbme</title>
+        <meta name="description" content="Subbmeについての問い合わせ" />
+      </Helmet>
+      <span>作成中</span>
+    </>
+  );
+}
