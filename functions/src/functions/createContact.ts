31c91ae0b7ac07b0f9fda6fccac2f110e3f15697
Tatsuki <private.1.5810@fastmail.jp>
2021-04-27 22:17:30 +0900

    axios追加

--- functions/src/functions/createContact.ts
+++ functions/src/functions/createContact.ts
@@ -1,10 +1,33 @@
 import { Contact } from "../entities/Contact";
 import * as functions from "firebase-functions";
 import { ContactManager } from "../repositories/Contacts";
+import axiosBase from "axios";
 import admin from "../libs/Firebase";
 
 const httpEvent = functions.region("us-central1").https.onCall(async (arg: { data: Contact, token: string}, context) => {
   const contact = arg.data;
+
+  const axios = axiosBase.create({
+    baseURL: "https://www.google.com",
+    // headers: {
+    //   'Content-Type': 'application/json',
+    //   'X-Requested-With': 'XMLHttpRequest'
+    // },
+    responseType: "json"
+  });
+  const verifyResult = await axios.post("/recaptcha/api/siteverify")
+  console.log("ver", verifyResult.data);
+  const isSuccess = verifyResult.data.success;
+  const errorCodes: string[] | undefined = verifyResult.data["error-codes"];
+  console.log("success", isSuccess);
+  console.log("error", errorCodes);
+
+  if (!isSuccess) {
+    console.error(verifyResult.data);
+    throw new Error("Faild challenge.");
+  }
+
+
   const manager = new ContactManager();
   const result = await manager.add(contact);
 
