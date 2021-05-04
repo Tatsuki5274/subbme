fcfe334c46737f19435d77d09f57b07145ba4ae6
Tatsuki <private.1.5810@fastmail.jp>
2021-05-04 20:35:58 +0900

    テストを削除

--- app/src/components/settings/pages/Settings.tsx
+++ app/src/components/settings/pages/Settings.tsx
@@ -1,9 +1,5 @@
 import SettingsTemplate from "../templates/SettingsTemplate";
-import firebase from "libs/Firebase";
 
 export default function Settings() {
-  const analytics = firebase.analytics();
-  analytics.logEvent("test2");
-  console.log(analytics);
   return <SettingsTemplate />;
 }
